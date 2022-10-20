bf1a05d0279ec645fd17020adaa435ab7c08b264
Lalit Kumar <31991060+lalit292929@users.noreply.github.com>
2022-10-20 17:43:25 +0530

    Create config.js

---
+++ utils/config.js
@@ -0,0 +1,11 @@
+export const config = {
+  REFRESH_TOKEN_SECRET: "abc123",
+  ACCESS_TOKEN_SECRET: "123abc",
+  localPoolConfig: {
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: "5432",
+    database: "lalitstealth",
+  }
+};
